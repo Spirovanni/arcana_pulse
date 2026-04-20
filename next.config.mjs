0a6932b939793a8791f4a56200fa550e71347415
Spirovanni <blackshieldsx@gmail.com>
2026-04-20 07:22:20 -0700

    fix: allow lh3.googleusercontent.com for Google profile images

--- next.config.mjs
+++ next.config.mjs
@@ -1,7 +1,16 @@
 import { withSentryConfig } from "@sentry/nextjs";
 
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: "https",
+        hostname: "lh3.googleusercontent.com",
+      },
+    ],
+  },
+};
 
 export default withSentryConfig(nextConfig, {
   // Suppresses source map upload logs during build
